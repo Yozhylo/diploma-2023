0f10dc4e5be7c4d1b375a991599d53a824af6079
Yozhylo <polidar2012@gmail.com>
2023-06-22 01:59:07 +0300

    Changes to scenes & new UI elements
    What a messy commit

--- Assets/Scripts/MenuUIController.cs
+++ Assets/Scripts/MenuUIController.cs
@@ -5,18 +5,35 @@ using UnityEngine.UIElements;
 
 public class MenuUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void OnEnable() {
-        var root = GetComponent<UIDocument>().rootVisualElement;
-        var buttonRoot = root.Q<VisualElement>("buttonHolder");
-        //var addAgentButton = root.Q<Button>("AddNewAgent");
-        var buttonStart = buttonRoot.Q<Button>("buttonStart");
-        buttonStart.clickable.clicked += () =>
-        {
-            saveValuesToGame();
-            SceneManager.LoadScene(1);
-        };
-        //addAgentButton.clicked += () => FishSpawner.createFish();
+  [SerializeField] private VisualTreeAsset asset;
+  // Start is called before the first frame update
+  private void OnEnable() {
+    var root = GetComponent<UIDocument>().rootVisualElement;
+    var biotope = root.Q<Foldout>("Biotope");
+    var buttonRoot = root.Q<VisualElement>("buttonHolder");
+    var addAgentButton = root.Q<Button>("AddNewAgent");
+    var buttonStart = buttonRoot.Q<Button>("buttonStart");
+    
+    
+    // int counter = 1;
+    addAgentButton.clickable.clicked += () => {
+      // biotope.Add(AgentRow);
+      // Debug.Log("button clockerd");
+      // Debug.Log(counter);
+
+      // Creating a new foldout from template on button click
+      // NEED TO MAKE IT NUMERATE FOLDOUTS BASED ON ORDER BEING ADDED
+      // OR PROPOSE SOME MAKESHIFT BULLSHIT TO MAKE INSTEAD
+      biotope.Add(asset.Instantiate());
+      var foldout = biotope.Q<TemplateContainer>("TemplateContainer").Q<Foldout>("DropdownField");
+      
+      // counter++;
+      // Debug.Log(counter);
+    };
+    buttonStart.clickable.clicked += () => {
+      saveValuesToGame();
+      SceneManager.LoadScene("Simulation");
+    };
     }
 
     void saveValuesToGame()
